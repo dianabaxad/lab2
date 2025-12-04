b550ee99425d70849cc453ad4c5bd316cdbd9da4
PC <dainka.smirnova.05@bk.ru>
2025-12-04 03:53:35 +0300

    инит

---
+++ geometry_package/__init__.py
@@ -0,0 +1,6 @@
+from geometry_package.rectangle import Rectangle
+from geometry_package.triangle import Triangle
+from geometry_package.trapezoid import Trapezoid
+
+__all__ = ['Rectangle', 'Triangle', 'Trapezoid']
+__version__ = '1.0.0'
